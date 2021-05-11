e349c15d2484c66bcf5c488a4b0f78705c307416
mirlz <mirlzy@gmail.com>
2021-05-11 16:16:34 +0800

    implement findByIdAndRemove

--- myApp.js
+++ myApp.js
@@ -95,8 +95,13 @@ const findAndUpdate = async(personName, done) => {
   }
 };
 
-const removeById = (personId, done) => {
-  done(null /*, data*/);
+const removeById = async(personId, done) => {
+  try {
+    const person = await Person.findByIdAndRemove(personId);
+    done(null, person);
+  } catch(e) {
+    done(e, null);
+  }
 };
 
 const removeManyPeople = (done) => {
